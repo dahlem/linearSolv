69ac94a8cd65e7e3dbd8db1ebdebc929d3aed676
Dominik Dahlem <Dominik.Dahlem@cs.tcd.ie>
2008-01-05 15:17:42 +0000

    Implemented and tested the Cholesky decomposition using the Cholesky-Crout reduction.

--- test/solvers/chol_test.c
+++ test/solvers/chol_test.c
@@ -62,40 +62,40 @@ void testCholCrout()
 
 void testChol()
 {
-/*     gsl_vector *x_bar, *x_error, *temp; */
-/*     double max_error; */
-/*     size_t i; */
+    gsl_vector *x_bar, *x_error, *temp;
+    double max_error;
+    size_t i;
     
-/*     double a_array[] = { */
-/*         1.86279, 0.47863, -0.54877, */
-/*         0.47863, 1.61609, 0.10628, */
-/*         -0.54877, 0.10628, 2.76115 */
-/*     }; */
+    double a_array[] = {
+        1.86279, 0.47863, -0.54877,
+        0.47863, 1.61609, 0.10628,
+        -0.54877, 0.10628, 2.76115
+    };
 
-/*     double b_array[] = { */
-/*         1, 2, 3 */
-/*     }; */
+    double b_array[] = {
+        1, 2, 3
+    };
     
-/*     double x_array[] = { */
-/*         1, 1, 1 */
-/*     }; */
+    double x_array[] = {
+        1, 1, 1
+    };
     
-/*     gsl_matrix_view A = gsl_matrix_view_array(a_array, 3, 3); */
-/*     gsl_vector_view b = gsl_vector_view_array(b_array, 3); */
-/*     gsl_vector_view x = gsl_vector_view_array(x_array, 3); */
+    gsl_matrix_view A = gsl_matrix_view_array(a_array, 3, 3);
+    gsl_vector_view b = gsl_vector_view_array(b_array, 3);
+    gsl_vector_view x = gsl_vector_view_array(x_array, 3);
 
-/*     CU_ASSERT_EQUAL(lu(&A.matrix, &b.vector, &x.vector, */
-/*                        &x_bar, &x_error, &max_error), 0); */
+    CU_ASSERT_EQUAL(chol(&A.matrix, &b.vector, &x.vector,
+                         &x_bar, &x_error, &max_error), 0);
 
-/*     temp = gsl_vector_calloc(x_bar->size); */
-/*     gsl_blas_dgemv(CblasNoTrans, 1.0, &A.matrix, x_bar, 0.0, temp); */
+    temp = gsl_vector_calloc(x_bar->size);
+    gsl_blas_dgemv(CblasNoTrans, 1.0, &A.matrix, x_bar, 0.0, temp);
 
-/*     for (i = 0; i < temp->size; ++i) { */
-/*         CU_ASSERT_DOUBLE_EQUAL( */
-/*             gsl_vector_get(&b.vector, i), */
-/*             gsl_vector_get(temp, i), */
-/*             0.01); */
-/*     } */
+    for (i = 0; i < temp->size; ++i) {
+        CU_ASSERT_DOUBLE_EQUAL(
+            gsl_vector_get(&b.vector, i),
+            gsl_vector_get(temp, i),
+            0.01);
+    }
 
-/*     gsl_vector_free(temp); */
+    gsl_vector_free(temp);
 }
